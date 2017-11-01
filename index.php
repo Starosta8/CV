15b298b3eb9f481a10b8594c3bbc77dd27f583c5
Alex Onoshenko <o.onoshenko@gmail.com>
2017-11-01 17:43:34 +0200

    u
    u

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <link href="CV_Styles.css" rel="stylesheet">
+    <meta name="description" content="Curriculum vitae junior php developer">
+    <meta name="keywords" content="Резюме, Биография, Curriculum Vitae, Хорошый сотрудник, Разработчик,
+     Junior php developer, php, php developer">
+    <title>CV_Alex_Onoshenko</title>
+</head>
+<body>
+<div id="div0">
+    <div class="div1" >
+        <h1>Junior php developer</h1>
+    </div>
+    <div id="img">
+        <img src="IMG_2258.JPG" align="left">
+    </div>
+    <div class="div2">
+        <h2>Александр Оношенко</h2>
+    </div>
+    <div class="div2">
+        <strong>Желаемая должность:</strong> Junior php developer
+    </div>
+    <div class="div2">
+        <strong>Желаемый уровень дохода:</strong> &nbsp500$
+    </div>
+    <div class="div2">
+         <b>Дата рождения:</b>&nbsp12/05/90
+    </div>
+    <div class="div2">
+        <b>Место проживания:</b>&nbspг.Киев
+    </div>
+    <div class="div2">
+        Готов к командировкам. Не готов к переезду.
+    </div>
+    <div class="div2">
+        <b>Контактная информация:</b>
+    </div>
+    <div class="div2">
+        Телефон: 093 767 22 56
+    </div>
+    <div class="div2">
+        E-mail: o.onoshenko@gmail.com
+    </div>
+    <div class="div2" align="left">
+        <b>Социальные сети:</b>
+    </div>
+    <div class="div02">
+        <ul>
+            <li>
+                <a href=https://www.facebook.com/a.onoshenko">  Facebook </a>
+            </li>
+            <li>
+                <a href="https://vk.com/lstarostal"> Vkontakte</a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/alexander_onoshenko/">Instagram</a>
+            </li>
+        </ul>
+    </div>
+    <div class="Clean">
+
+    </div>
+    <div class="div3">
+        <div  align="left">
+            <b>Ключивые навыки:</b>
+            <ul>
+                <li>Опыт работы PHP/MySQL</li>
+                <li>понимание принципов работы WEB (HTTP/REST API/DNS/etc...)</li>
+                <li>базовое знание SQL и умение писать запросы</li>
+                <li>умение работать с API</li>
+                <li>базовое владение HTML/CSS/JavaScript/jQuery</li>
+                <li>базвоый опыт работы с git</li>
+                <li>умение логически мыслить, быстро обучаться, решать нестандартные задачи</li>
+            </ul>
+        </div>
+        <div class="pading">
+            <b>Образование – Высшее</b><br>
+            <b>Киевский национальный университет строительства и архитектуры (Киев)</b><br>
+            Учеба с 09.2008 по 07.2013 (4 года 10 месяцев)<br>
+            Строительный факультет.<br>
+            Специальность:  Менеджмент организаций и администрирования<br>
+        </div>
+        <div class="pading">
+            <b>Курсы Web-програмирования "Php Academy" </b><br>
+            Специальность: Php developer 2018. сертификат
+        </div>
+        <div class="pading">
+            <b>Опыт работы</b><br>
+            Отсуцтвует
+        </div >
+        <div class="pading">
+            <b>Дополнительная информация:</b><br>
+            <b>Иностранные языки:</b> английский язык (технический) – средний уровень (B2).
+        </div>
+        <div>
+            <b>Знание ПК:</b> продвинутый пользователь
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
